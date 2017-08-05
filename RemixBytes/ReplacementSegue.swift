6dc105ae97689ec822b14b53160084813da75ab8
Dan Cutting <dan@cutting.io>
2017-08-05 23:25:09 +0100

    Cross-fade views.

--- RemixBytes/ReplacementSegue.swift
+++ RemixBytes/ReplacementSegue.swift
@@ -5,6 +5,13 @@ import UIKit
 class ReplacementSegue: UIStoryboardSegue {
     override func perform() {
         let window = source.view.window
-        window?.rootViewController = destination
+        window?.insertSubview(destination.view, belowSubview: source.view)
+        destination.view.alpha = 0.0
+        UIView.animate(withDuration: 0.2, animations: {
+            self.source.view.alpha = 0.0
+            self.destination.view.alpha = 1.0
+        }) { finished in
+            window?.rootViewController = self.destination
+        }
     }
 }
