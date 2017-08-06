fee6e9748395be2391a310ad784c1331f33bf5cb
Dan Cutting <dan@cutting.io>
2017-08-06 14:20:16 +0100

    Shouldn't be extracted just yet.

--- RemixBytes/Resources/UIViewable.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2017 cutting.io. All rights reserved.
-
-import UIKit
-
-extension Viewable {
-    var viewController: UIViewController? {
-        return self as? UIViewController
-    }
-}
